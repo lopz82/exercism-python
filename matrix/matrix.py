6a2033d58b8a1e7d62fbc8d2be60b559622765ff
lopz82 <jorge.lopez.guzman@outlook.es>
2021-01-02 12:24:37 +0100

    Updating solution

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -3,10 +3,10 @@ from typing import List
 
 class Matrix:
     def __init__(self, matrix_string: str):
-        self.matrix = [x.split() for x in matrix_string.split("\n")]
+        self.matrix = [[int(n) for n in x.split()] for x in matrix_string.split("\n")]
 
     def row(self, index: int) -> List[int]:
-        return [int(x) for x in self.matrix[index - 1]]
+        return [x for x in self.matrix[index - 1]]
 
     def column(self, index: int) -> List[int]:
-        return [int(x[index - 1]) for x in self.matrix]
+        return [x[index - 1] for x in self.matrix]
