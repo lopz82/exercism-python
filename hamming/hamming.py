ca6f36c124ab0f5bbe252c9030fc8133d040fe38
lopz82 <jorge.lopez.guzman@outlook.es>
2021-01-06 07:56:38 +0100

    Adding hamming exercise

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -3,4 +3,4 @@ def distance(strand_a: str, strand_b: str) -> int:
         raise ValueError(
             f"Strands must have same length: {len(strand_a)} != {len(strand_b)}"
         )
-    return sum([a != b for a, b in zip(strand_a, strand_b)])
+    return sum(a != b for a, b in zip(strand_a, strand_b))
